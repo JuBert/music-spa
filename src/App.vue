f1254fb62b56bea5a982e5250a77c9386e7f698d
JuBert <julien.aubert.rbs@gmail.com>
2023-09-23 14:31:04 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { Note, Interval, Scale, ScaleType, Chord } from 'tonal';
+
+const scaleNames = Scale.names();
+const note = 'c5';
+const scale = scaleNames?.[8];
+const scaleName = `${note} ${scale}`;
+const fullScale = Scale.get(scaleName);
+</script>
+
+<template>
+  <div>
+    <pre>Test</pre>
+    <pre>Scale name: {{ scale }}</pre>
+    <pre>Scale notes: {{ fullScale?.notes }}</pre>
+  </div>
+</template>
